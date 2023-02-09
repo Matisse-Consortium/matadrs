0bfab4557c90f4367e3ecf3e5d0ce4733f1e53d7
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2023-02-09 11:37:28 +0100

    Fix path to execute

--- examples/execute_pipeline.py
+++ examples/execute_pipeline.py
@@ -3,9 +3,9 @@ from pathlib import Path
 from matadrs import reduction_pipeline
 
 
-data_dir = Path("/data/beegfs/astro-storage/groups/matisse/scheuck/data/matisse/GTO/hd142666")
-observation_dirs = ["UTs/2022-04-23"]
+data_dir = Path("/data/beegfs/astro-storage/groups/matisse/scheuck/data/matisse/GTO/hd163296")
+observation_dirs = ["2019-03-22"]
 raw_dirs = list(map(lambda x: data_dir / "raw" / x, observation_dirs))
 product_dirs = list(map(lambda x: Path(str(x).replace("raw", "test")), raw_dirs))
 reduction_pipeline(raw_dirs, product_dirs, overwrite=True, do_reduce=True,
-                   do_calibrate=True, do_average=True, do_merge=True)
+                   do_calibrate=False, do_average=False, do_merge=False)
