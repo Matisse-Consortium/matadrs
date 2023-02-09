96e58127e1a391afb6bcf16f57a4d4b983640b98
Marten Scheuck <martenscheuck@gmail.com>
2023-02-09 14:45:02 +0100

    Fix mat_autoPipeline

--- examples/execute_pipeline.py
+++ examples/execute_pipeline.py
@@ -6,6 +6,6 @@ from matadrs import reduction_pipeline
 data_dir = Path("/data/beegfs/astro-storage/groups/matisse/scheuck/data/matisse/GTO/hd163296")
 observation_dirs = ["2019-03-22"]
 raw_dirs = list(map(lambda x: data_dir / "raw" / x, observation_dirs))
-product_dirs = list(map(lambda x: Path(str(x).replace("raw", "test")), raw_dirs))
-reduction_pipeline(raw_dirs, product_dirs, overwrite=True, do_reduce=True,
-                   do_calibrate=False, do_average=False, do_merge=False)
+product_dirs = list(map(lambda x: Path(str(x).replace("raw", "product")), raw_dirs))
+reduction_pipeline(raw_dirs, product_dirs, overwrite=True, do_reduce=False,
+                   do_calibrate=True, do_average=True, do_merge=True)
