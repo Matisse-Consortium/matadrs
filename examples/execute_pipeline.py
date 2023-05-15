dba64bd0875404f9fb01e46ac8a9de17b9909b24
Marten Scheuck <martenscheuck@gmail.com>
2023-05-15 17:41:05 +0200

    Update

--- examples/execute_pipeline.py
+++ examples/execute_pipeline.py
@@ -4,16 +4,20 @@ from matadrs import reduction_pipeline
 
 
 # Specify the path to the directory containing the data
-data_dir = Path("/data/beegfs/astro-storage/groups/matisse/scheuck/data/matisse/GTO/hd142666")
-observation_dirs = ["UTs/2022-04-21", "UTs/2022-04-23"]
+data_dir = Path("/data/beegfs/astro-storage/groups/matisse/scheuck/data/matisse/GTO/hd142666/")
+# observation_dirs = [data_dir / "raw/UTs/2022-04-21",
+                    # data_dir / "raw/UTs/2022-04-23"]
+
+observation_dirs = [data_dir / "raw/UTs/2019-05-13"]
 
 # Specify the raw-directory, containing the raw data
-raw_dirs = list(map(lambda x: data_dir / "raw" / x, observation_dirs))
+# raw_dirs = list(map(lambda x: data_dir / "raw" / x, observation_dirs))
 
 # Specify the product-directory, to contain the product data/that contains reduced,
 # calibrated or averaged data, to be further processed
-product_dirs = list(map(lambda x: Path(str(x).replace("raw", "product")), raw_dirs))
+product_dirs = list(map(lambda x: Path(str(x).replace("raw", "product")),
+                        observation_dirs))
 
 # Call the reduction_pipeline
-reduction_pipeline(raw_dirs, product_dirs, overwrite=True, do_reduce=False,
-                   do_calibrate=False, do_average=False, do_merge=True)
+reduction_pipeline(observation_dirs, product_dirs, overwrite=True, do_reduce=False,
+                   do_calibrate=True, do_average=False, do_merge=False)
--- examples/plotting.py
+++ examples/plotting.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from matadrs.utils.plot import Plotter
 
-file = Path("/Users/scheuck/Data/reduced_data/hd142666/midi/hd142666_2004-06-08T04:19:12_MIDI.fits")
+file = Path("/Users/scheuck/Data/reduced_data/")
 
 plotter = Plotter(file)
 plotter.add_uv().add_vis().plot()
