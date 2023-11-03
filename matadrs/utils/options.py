ba63029384e2c87906bb1d7ec990d7e01d72d851
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2023-11-03 15:17:32 +0100

    Fix typo

--- matadrs/utils/options.py
+++ matadrs/utils/options.py
@@ -1,5 +1,5 @@
 OPTIONS = {}
-OPTIONS["plot.COLORS"] = ["b", "g", "r", "c", "m", "y"]
+OPTIONS["plot.colors"] = ["b", "g", "r", "c", "m", "y"]
 OPTIONS["plot.linestyles"] = ["-", "--", "-.", ":"]
 
 OPTIONS["average.func"] = "biweightmean"
