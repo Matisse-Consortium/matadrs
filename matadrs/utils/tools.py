cedf1938ddef5b6e6069e3c07a2e5e049d457d01
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2024-03-11 13:16:48 +0100

    Add instrument adding function
    This function adds the instrument to datasets without, can also add synthetic errors

--- matadrs/utils/tools.py
+++ matadrs/utils/tools.py
@@ -347,3 +347,30 @@ def calculate_uv_tracks(baselines: List[np.ndarray],
                                                    * np.cos(declination))))
     return transform_uv_points(baselines, np.linspace(-hamax, hamax, 1000),
                                latitude_paranal, declination)
+
+
+def add_instrument(fits_file: List[Path], instrument: str,
+                   add_error: Optional[bool] = False) -> None:
+    """Add the instrument name to the header of the fits file
+    as as well as noise to the data.
+    """
+    with fits.open(fits_file, "update") as hdul:
+        hdul[0].header["INSTRUME"] = instrument.upper()
+        if add_error:
+            for key in ["vis", "t3"]:
+                if key == "vis":
+                    subhdr, errhdr = "visamp", "visamperr"
+                else:
+                    subhdr, errhdr = "t3phi", "t3phierr"
+
+                data = hdul[f"oi_{key}"].data[subhdr]
+                if key == "vis":
+                    noise = np.random.uniform(low=0, high=0.1, size=data.shape)
+                else:
+                    if np.any(data > 0):
+                        noise = np.random.uniform(low=0, high=5, size=data.shape)
+                    else:
+                        noise = np.zeros(data.shape)
+
+                hdul[f"oi_{key}"].data[errhdr] = noise
+        hdul.flush()
